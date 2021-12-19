bcb0872581c54d2a4b7a64a0bf733be04bd5f545
xdai02 <2513276112@qq.com>
2021-12-19 19:41:02 +0800

    convert to latex version

---
+++ Calculus/clean.py
@@ -0,0 +1,19 @@
+import os
+
+EXCLUSION = [
+    ".aux",
+    ".bcf",
+    ".idx",
+    ".log",
+    ".ptc",
+    ".xml",
+    ".gz",
+    ".toc"
+]
+
+for root, dirs, files in os.walk(os.getcwd()):
+    for file in files:
+        postfix = file[file.rfind("."):]
+        if postfix in EXCLUSION:
+            os.remove(os.path.join(root, file))
+            print("DELETED: " + file)
