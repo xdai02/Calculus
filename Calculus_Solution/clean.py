8b1eb0a97229db1fa596ead08f903f981e3b8ac1
xdai02 <2513276112@qq.com>
2022-01-05 11:08:16 +0800

    update clean.py

--- Calculus_Solution/clean.py
+++ Calculus_Solution/clean.py
@@ -8,12 +8,16 @@ EXCLUSION = [
     ".ptc",
     ".xml",
     ".gz",
-    ".toc"
+    ".toc",
+    ".out"
 ]
 
 for root, dirs, files in os.walk(os.getcwd()):
     for file in files:
         postfix = file[file.rfind("."):]
         if postfix in EXCLUSION:
-            os.remove(os.path.join(root, file))
-            print("DELETED: " + file)
+            try:
+                os.remove(os.path.join(root, file))
+                print("DELETED: " + file)
+            except:
+                pass
